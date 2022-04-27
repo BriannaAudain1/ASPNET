0c9c76ec856edc863fb06f4b1acac943592a43cc
Brianna Audain <briannaaudain2@outlook.com>
2022-04-27 11:33:56 -0500

    initial commit

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace Testing.Models
+﻿using System.Collections.Generic;
+
+namespace Testing.Models
 {
     public class Product
     {
@@ -7,6 +9,8 @@
         public double Price { get; set; }
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
-        public int StockLevel { get; set; }  
+        public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
     }
 }
